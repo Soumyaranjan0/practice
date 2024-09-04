56ac4ecfe9b16bcd00147aa2bf8061c4abccd3d3
soumyaranjan0 <soumyaranjanbehera012@gmail.com>
2024-09-04 21:40:21 +0530

    add some code

--- interview1.js
+++ interview1.js
@@ -22,7 +22,7 @@
 //         for(let j=0;j<arr[i].length;j++){
 //             flatenarr.push(arr[i][j])
 //         }
-//     } 
+//     }
 //     return flatenarr
 // }
 // console.log(flatarr(arr,1));
@@ -51,7 +51,6 @@
 // console.log(typeof NaN); //number
 
 // console.log("5"-"3"+6); //8
-
 
 //important
 // let arr=["z","a","d","b","e"]
@@ -94,7 +93,6 @@
 // let copy = JSON.parse(JSON.stringify(original));
 
 //difference between setTimeout() and setInterval()
-
 
 // Set an interval to log a message every second
 // let intervalId = setInterval(() => {
@@ -151,12 +149,11 @@
 //fragments in react:
 // Sometimes wrapping elements in a div or another HTML element can lead to unnecessary DOM nodes, which might affect the layout, styling, or performance. Fragments allow you to avoid this by grouping elements without adding extra nodes.
 
-
 //Temporary dead zone in javascript
 // Scope and Hoisting:
 // Variables declared with var are hoisted to the top of their scope and initialized with undefined, so they can be accessed before the actual declaration without causing an error (though they will be undefined).
 // Variables declared with let and const are also hoisted, but they are not initialized until the execution reaches the line of code where they are declared. This creates the TDZ.
-// eg 
+// eg
 // console.log(x); // ReferenceError: Cannot access 'x' before initialization //tdz
 // let x = 10;
 // console.log(x); // 10
@@ -170,5 +167,237 @@
 //diiference between usecallback and usememo hooks
 //html vrs xhtml
 //em tag vrs i tag
+
+// var a =12;
+// console.log(a*12)
+// console.log(a**12) //12^12
+
+// "use strict"
+// function test(){
+//     salary=12000;
+//     console.log(salary);
+// }
+// test()  //12000 //it is a wrong practice so you can write use scrict
+
+// function test(){
+//     console.log(test.abc);
+// }
+// test() //undefined
+// test.abc=400;
+// test.abc=600;
+// test() //600
+
+// console.log({}=={}); //false
+// console.log({}==={}); //false
+
+// let a={
+//     name : "soumya",
+// }
+// let b={
+//     ...a
+// }
+// b.name="dibya"
+// console.log(a.name);
+
+// let a =10;
+// let b = new Number(10)
+// let c =10;
+// console.log(a===b); //false
+// console.log(b===c); //false
+
+// function test(record){
+//     if(record=={age:28}){
+//         console.log('you are an adult');
+//     }else if(record==={age : 28}){
+//         console.log('you are still adult');
+//     }else{
+//         console.log('no record');
+//     }
+// }
+// test({ age : 28}) //no record //because of different memory location
+
+// console.log(+true); //1
+// console.log(!'soumya'); //false
+
+// var a = {};
+// var b = { key: "b" };
+// var c = { key: "c" };
+// a[b]=600;
+// b[c]=700;
+// console.log(a[c]); //600
+// console.log(a[b]); //600
+// console.log(b[b]); //700
+// console.log(b[c]); //700
+
+//method to convert a number to string is
+//JSON.stringify()
+//String(number)
+//number.toString()
+//number+""
+//`${numnber}`
+
+// let str="abcdef"
+// let str1=123;
+// a1b2c3def
+// let str="abc"
+// let str1=123456;
+// let str2=String(str1)
+// //a1b2c3456
+// function join(str,str2){
+//     let newstr=[]
+//     for(let i=0;i<Math.max(str.length,str2.length);i++){
+//         newstr.push(str[i],str2[i])
+//     }
+//     console.log(newstr.join(""));
+// }
+// join(str,str2)
+
+// or
+
+// let str = "abcdef";
+// let str1 = "123";
+// let result = '';
+// for (let i = 0; i < Math.max(str.length, str1.length); i++) {
+// //Ensures the loop runs as long as the longest of the two strings.
+//     if (i < str.length) {
+//         result += str[i];
+//     }
+//     if (i < str1.length) {
+//         result += str1[i];
+//     }
+// }
+// console.log(result); // Output: "a1b2c3def"
+
+// function fun1(){
+//     return 2
+// }
+// function fun2(){
+//     return 4
+// }
+// let a =(fun1(),fun2())
+// console.log(a); //4
+
+// let arr = ["one", "two", "three"];
+// let str = "hello";
+// const a = arr.includes("on");
+// const b = arr.includes("onetwo");
+// const c= str.includes('ll')
+// console.log(a,b,c) //false false true
+
+// console.log(true==""); //false
+// console.log("1"==1); //true
+
+// let a ={name:'Adarsha'}
+// let z={...a}
+// z.name="Adil"
+// console.log(a.name); //'Adarsha
+
+// console.log(+true); //1
+// console.log(!"xyz"); //false
+
+
+// Example of currying
+// function curry(a){
+//     return function (b){
+//         return function (c){
+//             return a*b*c
+//         }
+//     }
+// }
+// console.log(curry(7)(6)(2));
+
+
+// const str1="hello"
+// const str2="world1234"
+
+// function mergeTwostr(str1,str2){
+//     let  str3=[]
+//   for(let i=0;i<Math.max(str1.length,str2.length);i++){
+//     str3.push(str1[i],str2[i])
+//   }
+//   return str3.join("");
+// }
+// console.log(mergeTwostr(str1,str2));
+
+//write function for both the case it will return same value
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(2,4) //6
+// sum((2),(4)) 6
+
+// what is fragment in react?
+
+//lifecycle method in react?
+
+//diff between axois and fetch?
+//difference between strong and bold tag?
+//diff. between metertag and progress tag in html?
+//use of required attribute in html?
+//what is data list?
+//diff between em tag and i tag?
+//what is empty tag? which not closing tag like br ,hr etc
+//specificity of css
+//how can we add gradient in css
+//diff flexbox in grid?
+//what id and 2d?
+//what is css overflow?
+//property for image reflection?
+//attribute selectors?
+//psedo element in css?
+
+// function abc(){
+//     console.log(abc.timeout)
+// }
+// abc() //undefined
+// abc.timeout=200;
+// abc.timeout=100;
+// abc() //100
+
+// console.log(isNaN("hello"))    // true
+// console.log(isNaN(123));       // false
+// console.log(isNaN('123'));     // false
+// console.log(isNaN('abc'));     // true
+// console.log(isNaN(NaN));       // true
+// console.log(isNaN(undefined)); // true
+// For a stricter check, you can use Number.isNaN, which does not coerce the value.
+// console.log(Number.isNaN('abc')); // false
+// console.log(Number.isNaN(NaN));   // true
+
+// console.log(2-"2"); //0
+// console.log("2"-2); //0
+// console.log(2+'2') //22
+// console.log(2+ +'2') //4
+
+// console.log(typeof(NaN)) //number
+// console.log(NaN===NaN) //false //because of different memory location
+
+//which situation i will receive NaN as output?
+//console.log(0 / 0);          // NaN (undefined division)
+// console.log(Math.sqrt(-1));  // NaN (square root of a negative number)
+// console.log(Infinity - Infinity); // NaN (undefined subtraction)
+// console.log(parseInt('abc'));  // NaN
+// console.log(parseFloat('xyz')); // NaN
+// let result = NaN + 5;
+// console.log(result); // NaN
+// result = NaN * 2;
+// console.log(result); // NaN
+// console.log(Math.log(-1));      // NaN (logarithm of a negative number)
+// console.log(Math.acos(2));      // NaN (arc cosine out of range)
+// console.log(Math.pow(-1, 0.5)); // NaN (fractional power of a negative number)
+// console.log('abc' * 3); // NaN
+// console.log("abc"+3); //abc3 //in case of + only it will concat
+// console.log(Number(true)) // 1
+// console.log(Number("hello")) // NaN
+
+// console.log(a); //undefined
+// console.log(b); //ReferenceError: b is not defined
+// var a=b=6
+
+
+
+
+
+
 
 
